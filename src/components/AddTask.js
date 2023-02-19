7d693ba6f17cbc2b385b5b5300dfadfe419bbb4b
giorgialari <g.giorgialari@gmail.com>
2023-02-19 12:17:56 +0100

    add http request and routing

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,13 +2,24 @@ import React from 'react'
 import  {useState} from 'react'
 
 
-function AddTask() {
+function AddTask({onAdd}) {
   const [text, setText] = useState('')
   const [day, setDay] = useState('')
   const [reminder, setReminder] = useState(false)
 
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if(!text){
+      alert('Please add a task')
+      return
+    }
+    onAdd({text, day, reminder})
+    setText('')
+    setDay('')
+    setReminder(false)
+  }
   return (
-    <form className='add-form'>
+    <form className='add-form' onSubmit={onSubmit}>
         <div className='form-control'>
             <label>Task</label>
             <input type='text' placeholder='Add Task' value={text}
@@ -22,6 +33,7 @@ function AddTask() {
         <div className='form-control form-control-check'>
             <label>Set Reminder</label>
             <input type='checkbox'value={reminder}
+            checked={reminder}
             onChange={(e) => setReminder(e.currentTarget.checked)}/>
         </div>
         <input type='submit' value='Save Task' className='btn btn-block'></input>
